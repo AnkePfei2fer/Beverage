2aa5b65d6a2c8667bf06e0247dd7437ae3f9ab1b
Anke <pfeiffer.anke@outlook.com>
2021-10-15 17:11:20 +0200

    Replace alert with empty page and text element

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 import { createTitleComponent } from "./lib/titleComponent.js";
 import { createFormComponent } from "./lib/formComponent.js";
+import { createElement } from "./lib/elements.js";
 
 const appPage = document.querySelector("#app");
 
 const titleComponent = createTitleComponent();
 const formComponent = createFormComponent(popup);
 
+// function popup(name, drink) {
+//   alert(`Hi ${name}, here's your ${drink}. Enjoy!`);
+// }
+
 function popup(name, drink) {
-  alert(`Hi ${name}, here is your ${drink}. Enjoy!`);
+  appPage.removeChild(formComponent);
+  appPage.removeChild(titleComponent);
+  const outputElement = createElement("div", { className: "output" }, [
+    `Hi ${name}, here's your ${drink}. Enjoy!`,
+  ]);
+  appPage.append(outputElement);
 }
 
 appPage.append(titleComponent);
