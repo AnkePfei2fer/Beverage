e08a512f50ceeaef6d08e23689a356ea828edb07
Anke <pfeiffer.anke@outlook.com>
2021-10-15 11:49:53 +0200

    Add text field and button

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 import { createTitleComponent } from "./lib/titleComponent.js";
+import { createFormComponent } from "./lib/formComponent.js";
 
 const appPage = document.querySelector("#app");
+
 const titleComponent = createTitleComponent();
+const formComponent = createFormComponent(function (drink) {
+  alert("Hi, here is Your ${drink}");
+});
+
 appPage.append(titleComponent);
+appPage.append(formComponent);
